94f9829250eb7f541fc101060b25a0901d378ddb
dhiru-06 <65435462+dhiru-06@users.noreply.github.com>
2020-10-02 18:29:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server is running at ${PORT}`));
